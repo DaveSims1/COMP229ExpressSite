52f05103f3dcd4dbad5b5c188ef16b8601fd8945
David Sims <david.sims098@gmail.com>
2023-10-06 18:16:03 -0400

    routes and bootstrap

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', 
-  { title: 'Express',
-    h1: 'Main Heading' });
+  res.render('index', { title: 'Home' });
+});
+
+/* home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+/* about page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+/* projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+/* services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+/* contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
 });
 
 module.exports = router;
